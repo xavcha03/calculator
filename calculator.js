5fc92a62428944006bf3b86da36e3a441482cc59
xavcha03 <ch.xavier03@gmail.com>
2022-08-02 11:17:19 +0200

    Création du tableau de boutons en fonction de la maquette Figma

---
+++ calculator.js
@@ -0,0 +1,98 @@
+let buttonsList = [
+  {
+    type: "operation",
+    content: "X²",
+  },
+  {
+    type: "operation",
+    content: "√",
+  },
+  {
+    type: "operation",
+    content: "π",
+  },
+  {
+    type: "operation",
+    content: "(",
+  },
+  {
+    type: "operation",
+    content: ")",
+  },
+  {
+    type: "number",
+    content: 7,
+  },
+  {
+    type: "number",
+    content: 8,
+  },
+  {
+    type: "number",
+    content: 9,
+  },
+  {
+    type: "operation",
+    content: "x",
+  },
+  {
+    type: "operation",
+    content: "/",
+  },
+  {
+    type: "number",
+    content: 4,
+  },
+  {
+    type: "number",
+    content: 5,
+  },
+  {
+    type: "number",
+    content: 6,
+  },
+  {
+    type: "operation",
+    content: "+",
+  },
+  {
+    type: "operation",
+    content: "-",
+  },
+  {
+    type: "number",
+    content: 1,
+  },
+  {
+    type: "number",
+    content: 2,
+  },
+  {
+    type: "number",
+    content: 3,
+  },
+  {
+    type: "operation",
+    content: "C",
+  },
+  {
+    type: "operation",
+    content: "<=",
+  },
+  {
+    type: "operation",
+    content: "+-",
+  },
+  {
+    type: "number",
+    content: 0,
+  },
+  {
+    type: "operation",
+    content: ",",
+  },
+  {
+    type: "operation",
+    content: "=",
+  },
+];
