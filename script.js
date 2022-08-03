3b98c77ee66846df124eec9b4334291859a6ba0f
Xavier Charles <ch.xavier03@gmail.com>
2022-08-03 11:45:46 +0200

    Soucis de CORS à résoudre

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import Calculator from "./assets/js/calculator/main.js";
-
+import Calculator from "assets/js/calculator/main.js";
+console.log("check")
 let calculator = new Calculator(document.querySelector("#calculator"));
-
